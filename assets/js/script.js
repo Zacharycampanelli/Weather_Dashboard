91688cb344a579cf3a1305b98d8ef64dd6f85d55
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-27 18:43:29 -0500

    obtained nessary data for the current weather using onecall api on openweathermap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
-function getWeatherInfo(x, y) {
+function getWeatherInfo(x, y, city) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     x +
     "&lon=" +
     y +
-    "&appid=648c5d45e53cd2b401e2b5f578752208";
+    "&units=imperial&appid=648c5d45e53cd2b401e2b5f578752208";
+
+  var cityName;
+  var temperature;
+  var windSpeed;
+  var humidity;
+  var uvIndex;
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        temperature = data.current.temp;
+        windSpeed = data.current.wind_speed;
+        humidity = data.current.humidity;
+        uvIndex = data.current.uvi;
       });
     }
   });
@@ -21,14 +30,16 @@ function getCoordinates(city) {
     "&limit=1&appid=648c5d45e53cd2b401e2b5f578752208";
   var xCoord;
   var yCoord;
+  var cityName;
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           xCoord = data[0].lat;
           yCoord = data[0].lon;
-          console.log(xCoord, yCoord);
-          getWeatherInfo(xCoord, yCoord);
+          cityName = data[0].name;
+          console.log(cityName, xCoord, yCoord);
+          getWeatherInfo(xCoord, yCoord, cityName);
         });
       } else {
         alert("not found");
