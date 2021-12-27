1530cbe7136c998ab93a61c6a992cbfaacf1501f
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-27 17:33:30 -0500

    removed previous api call. Used geocoding api to get the coordinates of the searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-function getWeatherInfo(city) {
+function getCoordinates(city) {
   var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    // "https://api.openweathermap.org/data/2.5/weather?q=" +
+    // city +
+    // "&appid=648c5d45e53cd2b401e2b5f578752208";//
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
-    "&appid=648c5d45e53cd2b401e2b5f578752208";
+    "&limit=1&appid=648c5d45e53cd2b401e2b5f578752208";
+  var xCoord;
+  var yCoord;
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data, city);
+          xCoord = data[0].lat;
+          yCoord = data[0].lon;
+          console.log(xCoord, yCoord);
         });
       } else {
         alert("not found");
@@ -18,4 +25,4 @@ function getWeatherInfo(city) {
     });
 }
 
-getWeatherInfo("torrington");
+getCoordinates("torrington");
