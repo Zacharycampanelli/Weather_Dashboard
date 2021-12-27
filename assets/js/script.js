75fc7ffae61096fb66627944462ed863a757c653
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-27 17:56:16 -0500

    used the coordinates from geocoding to call openweathermap's one call api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
+function getWeatherInfo(x, y) {
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    x +
+    "&lon=" +
+    y +
+    "&appid=648c5d45e53cd2b401e2b5f578752208";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+}
+
 function getCoordinates(city) {
   var apiUrl =
-    // "https://api.openweathermap.org/data/2.5/weather?q=" +
-    // city +
-    // "&appid=648c5d45e53cd2b401e2b5f578752208";//
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&limit=1&appid=648c5d45e53cd2b401e2b5f578752208";
@@ -15,6 +28,7 @@ function getCoordinates(city) {
           xCoord = data[0].lat;
           yCoord = data[0].lon;
           console.log(xCoord, yCoord);
+          getWeatherInfo(xCoord, yCoord);
         });
       } else {
         alert("not found");
