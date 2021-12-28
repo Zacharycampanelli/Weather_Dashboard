95e8b0e348e731a3e24c9e44581b444ead502015
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-27 19:21:08 -0500

    displays current weather info on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+var currentWeatherEl = document.querySelector("#today");
+
+function displayCurrentWeather(city, temp, wind, humid, uv) {
+  // if no result
+
+  //currentWeatherEl.textContent = "";
+  var cityName = document.createElement("h2");
+  cityName.innerHTML = city;
+  currentWeatherEl.appendChild(cityName);
+
+  var temperature = document.createElement("li");
+  temperature.innerHTML = "Temp: " + temp + "&#8457";
+  currentWeatherEl.appendChild(temperature);
+
+  var windSpeed = document.createElement("li");
+  windSpeed.innerHTML = "Wind: " + wind + "MPH";
+  currentWeatherEl.appendChild(windSpeed);
+
+  var humidity = document.createElement("li");
+  humidity.innerHTML = "Humidity: " + humid + "%";
+  currentWeatherEl.appendChild(humidity);
+
+  var uvIndex = document.createElement("li");
+  uvIndex.innerHTML = "UV Index: " + uv;
+  currentWeatherEl.appendChild(uvIndex);
+
+  // currentWeatherEl.
+}
+
 function getWeatherInfo(x, y, city) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -6,7 +35,6 @@ function getWeatherInfo(x, y, city) {
     y +
     "&units=imperial&appid=648c5d45e53cd2b401e2b5f578752208";
 
-  var cityName;
   var temperature;
   var windSpeed;
   var humidity;
@@ -18,6 +46,7 @@ function getWeatherInfo(x, y, city) {
         windSpeed = data.current.wind_speed;
         humidity = data.current.humidity;
         uvIndex = data.current.uvi;
+        displayCurrentWeather(city, temperature, windSpeed, humidity, uvIndex);
       });
     }
   });
